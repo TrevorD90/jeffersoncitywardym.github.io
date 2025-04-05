dc9783613e4c8dfa212bdd49dad5600c22fced72
Trevor Davis <125104436+TrevorD90@users.noreply.github.com>
2025-04-04 21:39:48 -0500

    Adding a way to call the header by JS

---
+++ js/header.js
@@ -0,0 +1,24 @@
+console.log("loaded");
+class MainHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div id="top-box">
+                <div class="header-title">Jefferson City Ward</div>
+                <div class="header-bottom">
+                    <div class="nav-links">
+                        <a href="#">Home</a>
+                        <a href="#">Documents</a>
+                        <a href="pages/admin.html">Admin</a>
+                    </div>
+                    <div class="login-section">
+                        <span id="user-email"></span>
+                        <button onclick="signIn()">Sign In</button>
+                        <button onclick="signOut()">Sign Out</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define('main-header', MainHeader)
