922460eff1053659b4899f36796b80707fe83dab
Trevor Davis <125104436+TrevorD90@users.noreply.github.com>
2025-04-04 21:57:48 -0500

    Made two headers, one for the main and one for the other pages.

--- js/header.js
+++ js/header.js
@@ -22,3 +22,28 @@ class MainHeader extends HTMLElement {
 }
 
 customElements.define('main-header', MainHeader)
+
+
+class PageHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div id="top-box">
+                <div class="header-title">Jefferson City Ward</div>
+                <div class="header-bottom">
+                    <div class="nav-links">
+                        <a href="../index.html">Home</a>
+                        <a href="#">Documents</a>
+                        <a href="#">Admin</a>
+                    </div>
+                    <div class="login-section">
+                        <span id="user-email"></span>
+                        <button onclick="signIn()">Sign In</button>
+                        <button onclick="signOut()">Sign Out</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+}
+
+customElements.define('page-header', PageHeader)
